07ce361fc6a4b1511ce561ce2fe8fa56859561ab
Akshit Arora <akshitarora9211@gmail.com>
2020-09-15 19:36:40 +0530

    basic struture added

---
+++ src/components/Chat.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import "../css/chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import { SearchOutlined, AttachFile, MoreVert } from "@material-ui/icons/";
+function Chat() {
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>last seen at ..</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body"></div>
+      <div className="chat__footer"></div>
+    </div>
+  );
+}
+
+export default Chat;
